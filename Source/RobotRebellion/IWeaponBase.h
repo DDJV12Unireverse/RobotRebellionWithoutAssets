4a87176a0a5b15d9cd38cb480b8c5a5592b69b5e
SunlayGGX <sunlay@evotion.fr>
2017-02-17 22:00:16 -0500

    IWeaponBase created and added to project
    Tested and approuved
    
    Created the IWeaponBase that inherit from PrimitiveComponent because it is
    a rendered component that could have a collision box. I've done it because
    I cannot do a forward déclaration easily... (And I don't want to lose time
    on it...)

---
+++ Source/RobotRebellion/IWeaponBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/PrimitiveComponent.h"
+#include "IWeaponBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTREBELLION_API UIWeaponBase : public UPrimitiveComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
