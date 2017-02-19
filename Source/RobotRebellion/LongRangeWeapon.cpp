ba87469862ef52f498b7ea1af80dff3b0ec2479d
SunlayGGX <sunlay@evotion.fr>
2017-02-19 03:26:18 -0500

    Fixed problems
    Tested and approved
    
    Fixed problems with spawn.

--- Source/RobotRebellion/LongRangeWeapon.cpp
+++ Source/RobotRebellion/LongRangeWeapon.cpp
@@ -9,29 +9,32 @@
 ULongRangeWeapon::ULongRangeWeapon() :
     UIWeaponBase()
 {
-
+    m_canAttack = true;
 }
 
 void ULongRangeWeapon::cppAttack(ARobotRebellionCharacter* user)
 {
     PRINT_MESSAGE_ON_SCREEN(FColor::Cyan, "LongAtt");
-    if (canAttack() && m_projectileClass != NULL)
+    if (/*canAttack() &&*/ m_projectileClass != NULL)
     {
         // Retrieve the camera location and rotation
         FVector cameraLocation;
         FRotator muzzleRotation;
         user->GetActorEyesViewPoint(cameraLocation, muzzleRotation);
         
+        PRINT_MESSAGE_ON_SCREEN(FColor::Purple, "Compute fire");
 
         // m_muzzleOffset is in camera space coordinate => must be transformed to world space coordinate.
         const FVector MuzzleLocation = cameraLocation + FTransform(muzzleRotation).TransformVector(m_muzzleOffset);
         muzzleRotation.Pitch += LIFT_OFFSET; // lift the fire a little 
-        UWorld* const World = GetWorld();
+        UWorld* const World = user->GetWorld();
         if (World)
         {
             FActorSpawnParameters spawnParams;
             spawnParams.Owner = user;
             spawnParams.Instigator = user->Instigator;
+
+            PRINT_MESSAGE_ON_SCREEN(FColor::Green, "Ready To Spawn");
 
             // spawn a projectile
             AProjectile* const projectile = World->SpawnActor<AProjectile>(
@@ -43,13 +46,15 @@ void ULongRangeWeapon::cppAttack(ARobotRebellionCharacter* user)
 
             if (projectile)
             {
+                PRINT_MESSAGE_ON_SCREEN(FColor::Red, "Spawned");
+
                 projectile->setOwner(user);
 
                 // Fire
                 const FVector fireDirection = muzzleRotation.Vector();
                 projectile->InitVelocity(fireDirection);
 
-                waitForReloading();
+                //waitForReloading();
             }
         }
     }
