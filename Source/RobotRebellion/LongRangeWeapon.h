b0c840e690dd070b2fb90aaf337fc4f9927528a7
SunlayGGX <sunlay@evotion.fr>
2017-02-18 22:19:03 -0500

    Short and Long Range Weapon obj created
    Tested. Approuved
    
    I just created ShortRangeWeapon class and LongRangeWeapon class derived
    from UIWeaponBase.

---
+++ Source/RobotRebellion/LongRangeWeapon.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "IWeaponBase.h"
+#include "LongRangeWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTREBELLION_API ULongRangeWeapon : public UIWeaponBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/RobotRebellion/ShortRangeWeapon.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "IWeaponBase.h"
+#include "ShortRangeWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTREBELLION_API UShortRangeWeapon : public UIWeaponBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
