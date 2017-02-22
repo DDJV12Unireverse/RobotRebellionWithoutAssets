f0924ad6120ba0bbcd34e380beeb7de37d12add3
USHERBROOKE\bono1801 <olivier.bonnemains@usherbrooke.ca>
2017-02-21 19:59:29 -0500

    creation short Weapon ShapeComponent

--- Source/RobotRebellion/ShortRangeWeapon.cpp
+++ Source/RobotRebellion/ShortRangeWeapon.cpp
@@ -3,10 +3,56 @@
 #include "RobotRebellion.h"
 #include "ShortRangeWeapon.h"
 #include "RobotRebellionCharacter.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Components/CapsuleComponent.h"
+#include "UObjectGlobals.h"
 
+/************************************************************************/
+/*                  CONSTRUCTOR                                           */
+/************************************************************************/
+UShortRangeWeapon::UShortRangeWeapon():UWeaponBase()
+{
+    //this->AttachChildren(UCapsuleComponent(50.f, 100.f)));
+    m_attackZone= CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+    m_attackZone->InitCapsuleSize(500.f, 1000.f);
+    m_attackZone->SetRelativeLocation(FVector(10.f, 0.f, 0.f));
+    m_attackZone->SetRelativeScale3D(FVector(1.f, 1.f, 1.f));
+    m_attackZone->bGenerateOverlapEvents = true;
+    m_attackZone->OnComponentBeginOverlap.AddDynamic(this, &UShortRangeWeapon::OnBeginOverlap);
+}
 
+/************************************************************************/
+/*                  UFUNCTION                                           */
+/************************************************************************/
+
+void UShortRangeWeapon::OnBeginOverlap(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+    PRINT_MESSAGE_ON_SCREEN(FColor::Cyan, "Contact MotherFucker");
+
+}
+
+/************************************************************************/
+/*                  METHODS                                           */
+/************************************************************************/
 void UShortRangeWeapon::cppAttack(ARobotRebellionCharacter* user)
 {
     PRINT_MESSAGE_ON_SCREEN(FColor::Cyan, "ShortAtt");
+
+    //Sphere for short range collision
+    FVector MultiSphereStart = user->GetActorLocation() + 5.f*user->GetActorForwardVector();
+    FVector MultiSphereEnd = MultiSphereStart + FVector(0.f, 0.f, 15.f);
+    //Considered Actors
+    TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+    ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
+    //Ignored actors, only user
+    TArray<AActor*> ActorsToIgnore;
+    ActorsToIgnore.Add(user);
+    //Result
+    TArray<FHitResult> OutHits;
+    bool Result = UKismetSystemLibrary::SphereTraceMultiForObjects(user->GetWorld(), MultiSphereStart, MultiSphereEnd, 5.0f*user->GetActorForwardVector().Size(), ObjectTypes, false, ActorsToIgnore, EDrawDebugTrace::ForDuration, // Le type de Debug
+        OutHits, // Où seront stockés les résultats
+        true);
+   
+    
 }
 
--- Source/RobotRebellion/ShortRangeWeapon.h
+++ Source/RobotRebellion/ShortRangeWeapon.h
@@ -13,11 +13,23 @@ class ROBOTREBELLION_API UShortRangeWeapon : public UWeaponBase
 {
 	GENERATED_BODY()
 	
+public:
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack Collision")
+        UCapsuleComponent* m_attackZone;
 	
 public:
     /************************************************************************/
+    /*                  CONSTRUCTOR                                           */
+    /************************************************************************/
+    UShortRangeWeapon();
+
+    /************************************************************************/
     /*                  UFUNCTION                                           */
     /************************************************************************/
+    UFUNCTION()
+        void OnBeginOverlap(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+    
     UFUNCTION(BlueprintCallable, Category = "General")
         EWeaponRange getWeaponRange() const USE_NOEXCEPT
     {
