cf4a490a3998e00139558feb22207684de10904e
SunlayGGX <sunlay@evotion.fr>
2017-02-27 17:15:24 -0500

    Drone AI controller object created
    Not tested. does nothing. Compiles

---
+++ Source/RobotRebellion/DroneAIController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CustomAIControllerBase.h"
+#include "DroneAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTREBELLION_API ADroneAIController : public ACustomAIControllerBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
