5d71e3abd4dbba4a06933502a69b3ad74495363a
USHERBROOKE\maie1703 <eric.maines@usherbrooke.ca>
2017-02-27 14:21:30 -0500

    Ennemy AI basics

---
+++ Source/RobotRebellion/CustomAIControllerBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AIController.h"
+#include "CustomAIControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTREBELLION_API ACustomAIControllerBase : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/RobotRebellion/EnnemiAIController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CustomAIControllerBase.h"
+#include "EnnemiAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTREBELLION_API AEnnemiAIController : public ACustomAIControllerBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
