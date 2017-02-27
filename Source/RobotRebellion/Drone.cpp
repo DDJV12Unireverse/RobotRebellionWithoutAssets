80bde0eb57aa28e1a576bc65922c8e2b1f7e56c5
SunlayGGX <sunlay@evotion.fr>
2017-02-27 17:02:17 -0500

    Base drone object
    Not Tested. Does nothing but compiles
    
    Just the C++ object creation.

---
+++ Source/RobotRebellion/Drone.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RobotRebellion.h"
+#include "Drone.h"
+
+
+ADrone::ADrone() : ANonPlayableCharacter()
+{
+    this->setImmortal(true);
+}
+
---
+++ Source/RobotRebellion/Drone.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NonPlayableCharacter.h"
+#include "Drone.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTREBELLION_API ADrone : public ANonPlayableCharacter
+{
+	GENERATED_BODY()
+	
+	
+public:
+    ADrone();
+};
