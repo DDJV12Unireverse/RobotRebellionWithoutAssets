ecd146558d932303404c08d1aaa8a4f969f36114
USHERBROOKE\maie1703 <eric.maines@usherbrooke.ca>
2017-02-28 11:32:27 -0500

    First version of Pickup actor added.

---
+++ Source/RobotRebellion/PickupActor.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RobotRebellion.h"
+#include "PickupActor.h"
+
+
+// Sets default values
+APickupActor::APickupActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APickupActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APickupActor::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
