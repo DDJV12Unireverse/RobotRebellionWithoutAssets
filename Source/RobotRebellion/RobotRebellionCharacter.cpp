98501ab924e78e0688242831ce90a8e6ed734dba
SunlayGGX <sunlay@evotion.fr>
2017-03-01 23:49:05 -0500

    Added methods to RobotRebelionCharacter
    Not tested. Compiles
    
    Added methods and properties to impolement billboard

--- Source/RobotRebellion/RobotRebellionCharacter.cpp
+++ Source/RobotRebellion/RobotRebellionCharacter.cpp
@@ -7,24 +7,44 @@
 #include "WeaponBase.h"
 #include "WeaponInventory.h"
 
+#include "TextBillboardComponent.h"
+#include "PlayableCharacter.h"
+
 
 #include "UtilitaryMacros.h"
+#include "UtilitaryFunctionLibrary.h"
 
 
 
 ARobotRebellionCharacter::ARobotRebellionCharacter()
 {
+    PrimaryActorTick.bCanEverTick = true;
+
     // Set size for collision capsule
     GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
     m_attribute = CreateDefaultSubobject<UAttributes>(TEXT("Attributes"));
 
-    //m_weaponInventory = CreateDefaultSubobject<UWeaponInventory>(TEXT("WeaponInventory"));
+    m_textBillboardDefault = CreateDefaultSubobject<UTextBillboardComponent>(TEXT("Default Billboard Component"));
 }
 
 void ARobotRebellionCharacter::BeginPlay()
 {
     Super::BeginPlay();
+}
+
+void ARobotRebellionCharacter::Tick(float deltaTime)
+{
+    Super::Tick(deltaTime);
+
+    if (m_textBillboardInstance)
+    {
+        m_textBillboardInstance->update(deltaTime);
+    }
+    else
+    {
+        this->createTextBillboard();
+    }
 }
 
 ///// SERVER
@@ -65,3 +85,26 @@ bool ARobotRebellionCharacter::clientOnDeath_Validate()
 
 void ARobotRebellionCharacter::cppOnRevive()
 {}
+
+void ARobotRebellionCharacter::displayAnimatedIntegerValue(int32 valueToDisplay, const FColor& color)
+{
+    m_textBillboardInstance->beginDisplayingInteger(this->GetActorLocation(), valueToDisplay, color);
+}
+
+void ARobotRebellionCharacter::displayAnimatedTextValue(const FString& textToDisplay, const FColor& color)
+{
+    m_textBillboardInstance->beginDisplayingText(this->GetActorLocation(), textToDisplay, color);
+}
+
+void ARobotRebellionCharacter::createTextBillboard()
+{
+    auto camera = Cast<APlayableCharacter>(GetWorld()->GetFirstPlayerController()->GetPawn())->FollowCamera;
+
+    if (UUtilitaryFunctionLibrary::createObjectFromDefault<UTextBillboardComponent>(&m_textBillboardInstance, m_textBillboardDefault, camera, RF_Dynamic))
+    {
+        m_textBillboardInstance->AttachToComponent(camera, FAttachmentTransformRules::KeepWorldTransform);
+        m_textBillboardInstance->Activate();
+
+        m_textBillboardInstance->RegisterComponent();
+    }
+}
--- Source/RobotRebellion/RobotRebellionCharacter.h
+++ Source/RobotRebellion/RobotRebellionCharacter.h
@@ -20,8 +20,15 @@ public:
         float m_moveSpeed;
 
      ////Weapon Inventory/////
-     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
-         class UWeaponInventory* m_weaponInventory;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+        class UWeaponInventory* m_weaponInventory;
+
+    ////Billboard on character////
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BillBoard")
+        class UTextBillboardComponent* m_textBillboardDefault;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "BillBoard")
+        class UTextBillboardComponent* m_textBillboardInstance;
 
 
 protected:
@@ -30,28 +37,49 @@ protected:
 
 
 public:
-
-    ARobotRebellionCharacter();
     /************************************************************************/
     /* METHODS                                                              */
     /************************************************************************/
+
+
+    ARobotRebellionCharacter();
+
+
     class UWeaponBase* getCurrentEquippedWeapon() const USE_NOEXCEPT;
 
-     virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
-    ////Server
-     void GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const override;
+    virtual void Tick(float deltaTime) override;
+
+
+   ////Server
+    void GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const override;
+
 
     virtual void cppOnRevive();
 
     virtual void cppOnDeath();
 
 
-     UFUNCTION()
-         void onDeath();
 
-     UFUNCTION(Reliable, Client, WithValidation)
-         void clientOnDeath();
+    /************************************************************************/
+    /* UFUNCTION                                                            */
+    /************************************************************************/
+
+    UFUNCTION()
+        void onDeath();
+
+    UFUNCTION(Reliable, Client, WithValidation)
+        void clientOnDeath();
+
+    UFUNCTION(BlueprintCallable, Category = "Billboard - Living Text")
+        void createTextBillboard();
+
+    UFUNCTION(BlueprintCallable, Category = "Billboard - Living Text")
+        void displayAnimatedIntegerValue(int32 valueToDisplay, const FColor& color);
+
+    UFUNCTION(BlueprintCallable, Category = "Billboard - Living Text")
+        void displayAnimatedTextValue(const FString& textToDisplay, const FColor& color);
 
 
 
