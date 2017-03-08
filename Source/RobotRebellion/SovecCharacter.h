e8c2c0fc88b97fb25657360df321f3144780a284
Constant Brunel <constant.95@gmail.com>
2017-03-08 12:33:21 -0500

    Add sovec Character

---
+++ Source/RobotRebellion/SovecCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NonPlayableCharacter.h"
+#include "SovecCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTREBELLION_API ASovecCharacter : public ANonPlayableCharacter
+{
+	GENERATED_BODY()
+	
+public:
+    ASovecCharacter();
+	
+	
+};
