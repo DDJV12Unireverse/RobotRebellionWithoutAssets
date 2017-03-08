9d9271a3f66971ebb71cf5f0996c85a673d1c890
SunlayGGX <sunlay@evotion.fr>
2017-03-08 17:45:36 -0500

    ???
    ???

---
+++ Source/RobotRebellion/IdentifiableObj.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RobotRebellion.h"
+#include "IdentifiableObj.h"
+
+IdentifiableObj::IdentifiableObj()
+{
+}
+
+IdentifiableObj::~IdentifiableObj()
+{
+}
---
+++ Source/RobotRebellion/IdentifiableObj.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class ROBOTREBELLION_API IdentifiableObj
+{
+public:
+	IdentifiableObj();
+	~IdentifiableObj();
+};
