5a5d0cf68b7dbda8e92335b2cbb359164d0c8a77
Constant Brunel <constant.95@gmail.com>
2017-03-20 17:14:44 -0400

    Add restore mana effect

---
+++ Source/RobotRebellion/Gameplay/Spell/RestoreManaEffect.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Gameplay/Spell/Effect.h"
+#include "RestoreManaEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTREBELLION_API URestoreManaEffect : public UEffect
+{
+	GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ManaEffect)
+        float m_manaGiven;
+
+public:
+    // Called when the game starts
+    virtual void BeginPlay() override;
+    // Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    // The behavior of the effect when it's a targeted effect
+    virtual void exec(class ARobotRebellionCharacter* caster, ARobotRebellionCharacter* target) override;
+    // The behavior of the effect when it's point effect
+    virtual void exec(const FVector impactPoint) override;
+	
+};
