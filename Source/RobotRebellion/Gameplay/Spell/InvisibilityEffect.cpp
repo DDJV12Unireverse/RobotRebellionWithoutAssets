2be20b3f751768447e31273951c41c8a1c56e002
Constant Brunel <constant.95@gmail.com>
2017-03-21 12:09:01 -0400

    Add invisibility effect/spell

---
+++ Source/RobotRebellion/Gameplay/Spell/InvisibilityEffect.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RobotRebellion.h"
+#include "InvisibilityEffect.h"
+
+#include "../../Character/RobotRebellionCharacter.h"
+
+
+void UInvisibilityEffect::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void UInvisibilityEffect::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UInvisibilityEffect::exec(ARobotRebellionCharacter* caster, ARobotRebellionCharacter* target)
+{
+    caster->inflictInvisibility();
+}
+
+void UInvisibilityEffect::exec(const FVector impactPoint)
+{
+}
