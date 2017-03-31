abb39a30afa1310b29bdca595f395108440ae035
USHERBROOKE\bono1801 <olivier.bonnemains@usherbrooke.ca>
2017-03-31 14:59:39 -0400

    long range weapon wizard done

--- Source/RobotRebellion/Gameplay/Weapon/FireProjectile.cpp
+++ Source/RobotRebellion/Gameplay/Weapon/FireProjectile.cpp
@@ -30,7 +30,29 @@ void AFireProjectile::OnHit(class UPrimitiveComponent* ThisComp, class AActor* O
         {
             if (!receiver->isImmortal())
             {
-                receiver->inflictDamage(m_baseDamage);
+                DamageCoefficientLogic coeff;
+
+                /*UUtilitaryFunctionLibrary::randomApplyObjectMethod<1>(
+                true,
+                coeff,
+                &DamageCoefficientLogic::criticalHit,
+                &DamageCoefficientLogic::engagementHit,
+                &DamageCoefficientLogic::superEfficient,
+                &DamageCoefficientLogic::lessEfficient,
+                &DamageCoefficientLogic::multipleHit,
+                &DamageCoefficientLogic::graze
+                );
+
+                PRINT_MESSAGE_ON_SCREEN_UNCHECKED(FColor::Cyan, FString::Printf(TEXT("Coefficient value at : %f"), coeff.getCoefficientValue()));*/
+
+                Damage damage{ m_owner, receiver };
+
+                Damage::DamageValue currentDamage = damage(
+                    &UGlobalDamageMethod::normalHitWithWeaponComputed,
+                    coeff.getCoefficientValue()
+                );
+
+                receiver->inflictDamage(m_baseDamage*currentDamage);
             }
             else
             {
