7de3f201ac4c97a6c77f2d296f8526a4b03de99e
USHERBROOKE\bono1801 <olivier.bonnemains@usherbrooke.ca>
2017-04-21 19:42:31 -0400

    creation of widget for life bar

---
+++ Source/RobotRebellion/UI/LifeBarWidget.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UI/RobotRebellionWidget.h"
+#include "LifeBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTREBELLION_API ULifeBarWidget : public URobotRebellionWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
