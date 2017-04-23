1933e1389e17e920f6ec6f028e187d9c4d4f6579
SunlayGGX <sunlay@evotion.fr>
2017-04-23 03:17:51 -0400

    Boss Robot lauch game music when spawned
    Tested. OK (... Editor buggy)

--- Source/RobotRebellion/Character/BossRobot.cpp
+++ Source/RobotRebellion/Character/BossRobot.cpp
@@ -11,6 +11,19 @@ ABossRobot::ABossRobot()
     m_weaponInventory = CreateDefaultSubobject<UWeaponInventory>(TEXT("WeaponInventory"));
 }
 
+void ABossRobot::BeginPlay()
+{
+    Super::BeginPlay();
+
+    UWorld* world = this->GetWorld();
+    TArray<AActor*> entity;
+    UGameplayStatics::GetAllActorsOfClass(world, AWorldInstanceEntity::StaticClass(), entity);
+    if(entity.Num() > 0)
+    {
+        Cast<AWorldInstanceEntity>(entity[0])->setBossGameMode();
+    }
+}
+
 void ABossRobot::cppOnDeath()
 {
     UWorld* w = this->GetWorld();
