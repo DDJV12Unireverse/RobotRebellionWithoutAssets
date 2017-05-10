a14933d7ad80fe5c45e2d93750135a65d44bdd6d
SunlayGGX <sunlay@evotion.fr>
2017-05-10 11:09:20 -0400

    Const added to world instance entity
    Tested. OK

--- Source/RobotRebellion/Global/WorldInstanceEntity.h
+++ Source/RobotRebellion/Global/WorldInstanceEntity.h
@@ -96,17 +96,17 @@ public:
 // Sets default values for this actor's properties
     AWorldInstanceEntity();
 
-    bool getGameStarted()
-    {
-        return m_gameIsStarted;
-    }
-
 
     // Called when the game starts or when spawned
     virtual void BeginPlay() override;
 
     // Called every frame
     virtual void Tick(float DeltaSeconds) override;
+
+    FORCEINLINE bool getGameStarted() const USE_NOEXCEPT
+    {
+        return m_gameIsStarted;
+    }
 
     void setBossGameMode();
 
@@ -137,12 +137,12 @@ public:
     UFUNCTION(Reliable, NetMulticast)
         void multiSetupAudioComponents();
 
-    const bool IsBurnEffectEnabled()
+    FORCEINLINE bool IsBurnEffectEnabled() const USE_NOEXCEPT
     {
         return m_isBurnEffectEnabled;
     }
 
-    void setIsBurnEffectEnabled(bool enable)
+    FORCEINLINE void setIsBurnEffectEnabled(bool enable)
     {
         m_isBurnEffectEnabled = enable;
     }
